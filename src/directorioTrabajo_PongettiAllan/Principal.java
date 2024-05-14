a6b5f4c2d93cfcb2bc727c7ac6bd408b8c1a44de
unknown <DAM120@iesataulfo.local>
2024-05-14 16:57:19 +0200

    Fase 2 proyecto(Clase creada y Principal creada y comentada)

--- src/directorioTrabajo_PongettiAllan/Principal.java
+++ src/directorioTrabajo_PongettiAllan/Principal.java
@@ -1,7 +1,9 @@
 package directorioTrabajo_PongettiAllan;
-
+/**
+ * creamos el método main que almacenara el objeto de clase y lo ejectutara para mostrar algo 
+ */
 public class Principal {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
